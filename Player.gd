6c19004a7e93de6d472ad36d01f61ea034215c7e
Aditya Rahmanda <arahmanda21@gmail.com>
2023-08-06 01:05:10 +0700

    implement jump forgiveness and add assets

--- Player.gd
+++ Player.gd
@@ -1,25 +1,26 @@
 extends CharacterBody2D
 
-
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var facing_right = true
+var isJumping = false
+var jumpForgiveness = 0.2
+var ungroundedTime = 0.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
+	if is_on_floor():
+		ungroundedTime = 0
+		isJumping = false
+	else:
 		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		ungroundedTime += delta
+		
+	if Input.is_action_just_pressed("ui_accept") and ungroundedTime <= jumpForgiveness and !isJumping:
 		velocity.y = JUMP_VELOCITY
+		isJumping = true
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction > 0:
 		$Sprite2D.scale.x = 1
@@ -28,9 +29,18 @@ func _physics_process(delta):
 	
 	if direction:
 		velocity.x = direction * SPEED
-		$AnimationPlayer.play("Walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		$AnimationPlayer.play("Idle")
+		
+	if isJumping:
+		if velocity.y > 0:
+			$AnimationPlayer.play("Jump_Up")
+		elif velocity.y < 0:
+			$AnimationPlayer.play("Jump_Down")
+	else:
+		if direction:
+			$AnimationPlayer.play("Walk")
+		else:
+			$AnimationPlayer.play("Idle")
 
 	move_and_slide()
